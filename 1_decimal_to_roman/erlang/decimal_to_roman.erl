0a2294559ce34926da8617bbb3e557b93c771f63
Santiago Munín González <burning1@gmail.com>
2012-04-23 00:44:09 +0200

    Fixed comments, again

--- 1_decimal_to_roman/erlang/decimal_to_roman.erl
+++ 1_decimal_to_roman/erlang/decimal_to_roman.erl
@@ -2,18 +2,19 @@
 -export([dec_to_roman/1]).
 
 % dec_to_roman/1 : integer -> string
-%   Map between decimal and romans
-dec_to_roman(Num) ->
+dec_to_roman(Num) when Num >= 1, Num =< 3999 ->
     dec_to_roman(Num, "",
         [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
         ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
-         "V", "IV", "I"]).
+         "V", "IV", "I"]);
+dec_to_roman(_) ->
+	io:format('Wrong number, please introduce a value between 1 and 3999.\n').
 
-% dec_to_roman/4 : integer string number (list string) -> string
+% dec_to_roman/4 : integer string (number list) (string list) -> string
 %   Decimal to roman converter.
 dec_to_roman(0, Acc, _Decimal, _Roman) -> Acc;
 dec_to_roman(Num, Acc, Decimal, Roman) ->
-	dec_head = hd(Decimal),
-	if dec_head =< Num -> dec_to_roman(Num - dec_head, Acc ++ hd(Roman), Decimal, Roman);
+	Dec_head = hd(Decimal),
+	if Dec_head =< Num -> dec_to_roman(Num - Dec_head, Acc ++ hd(Roman), Decimal, Roman);
 	true -> dec_to_roman(Num, Acc, tl(Decimal), tl(Roman))
 	end.	
